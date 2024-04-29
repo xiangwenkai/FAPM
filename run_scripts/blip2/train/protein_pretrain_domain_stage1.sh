9a28773d69306157d03504bba9cd10317cef18ad
xiangwenkai <icoxia@gmail.com>
2024-04-29 09:53:16 +0800

    fix

--- run_scripts/blip2/train/protein_pretrain_domain_stage1.sh
+++ run_scripts/blip2/train/protein_pretrain_domain_stage1.sh
@@ -1,16 +1,2 @@
 #!/bin/bash 
-#SBATCH -J c1
-#SBATCH -p gpu1
-#SBATCH -N 1
-#SBATCH -w node[84]
-#SBATCH --mem 60G
-#SBATCH --gres=gpu:6
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=icoxia@gmail.com
-#SBATCH --output=log.%j.out 
-#SBATCH --error=log.%j.err 
-#SBATCH --cpus-per-task=8
-module load anaconda3/2021.05 
-source activate LAVIS
-
-python -m torch.distributed.run --nproc_per_node=6 train.py --cfg-path lavis/projects/blip2/train/protein_pretrain_stage1.yaml
+python -m torch.distributed.run --nproc_per_node=4 train.py --cfg-path lavis/projects/blip2/train/protein_pretrain_stage1.yaml
